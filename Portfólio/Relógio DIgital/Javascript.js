1aa46a2f1acdf14c7a46f3cc68a51dbccf764334
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-02-10 00:06:55 -0300

    att
    att

--- Portfólio/Relógio DIgital/Javascript.js
+++ Portfólio/Relógio DIgital/Javascript.js
@@ -13,19 +13,3 @@ const relogio = setInterval(function time() {
     minutos.textContent = min;
     segundos.textContent = s;
 })
-var data = new Date();
-var hr = dateToday.getHours()
-var mudar = document.getElementsByClassName('mudar')
- function background(){
-        if (hr >= 10 && hr < 17){
-            body.style.background.src = 'estilonoite.jpg'
-
-        }
-        else if(hr >= 18 && hr < 23){
-            mudar.src = 'amanhecer.jpg'
-            
-        }
-        else{
-            body.style.background.src =  '../amanhecer.jpg'
-        }
-}
